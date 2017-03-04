77d84a150f8a686405f8415f69bb482c779911d5
Sumit Agrawal <sumitagrawal4548@gmail.com>
2017-03-04 11:27:22 +0530

    SpringLearningYoutube First Commit

---
+++ spring/src/main/java/com/sumit/learning/spring/spring/DrawingApp.java
@@ -0,0 +1,27 @@
+package com.sumit.learning.spring.spring;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Triangle t = new Triangle();
+		
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		
+		Triangle t = (Triangle) context.getBean("triangle");
+		t.draw();
+		
+		
+
+	}
+
+}
---
+++ spring/src/main/java/com/sumit/learning/spring/spring/Triangle.java
@@ -0,0 +1,9 @@
+package com.sumit.learning.spring.spring;
+
+public class Triangle {
+	
+	public void draw(){
+		System.out.println("trangle drawn");
+	}
+
+}
