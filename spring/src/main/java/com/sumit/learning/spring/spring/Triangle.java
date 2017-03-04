f9841ccd4b88cbea1c27f73b8903e7540d806490
Sumit Agrawal <sumitagrawal4548@gmail.com>
2017-03-04 11:35:40 +0530

    ApplicationContext
    the type of triangle is getting value from spring xml

--- spring/src/main/java/com/sumit/learning/spring/spring/Triangle.java
+++ spring/src/main/java/com/sumit/learning/spring/spring/Triangle.java
@@ -1,9 +1,19 @@
 package com.sumit.learning.spring.spring;
 
 public class Triangle {
-	
-	public void draw(){
-		System.out.println("trangle drawn");
+
+	private String type;
+
+	public void draw() {
+		System.out.println(getType() + "   trangle drawn");
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 }
